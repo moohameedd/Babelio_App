d346830d12496a2b9fd76094cbbb18a4880e4c1a
Oussaid <oussaid.dridi.0@gmail.com>
2026-03-26 19:57:03 +0100

    Initial commit - Android login UI

---
+++ app/src/main/java/com/example/tp/activity_splash.java
@@ -0,0 +1,24 @@
+package com.example.tp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class activity_splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        }, 2000);
+    }
+}
---
+++ app/src/main/java/com/example/tp/login.java
@@ -0,0 +1,35 @@
+package com.example.tp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class login extends AppCompatActivity {
+    ImageView btnClose;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+
+
+        btnClose = findViewById(R.id.btnClose);
+
+        btnClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goBack = new Intent(login.this, MainActivity.class);
+                startActivity(goBack);
+                finish();
+            }
+        });
+
+    }
+}
