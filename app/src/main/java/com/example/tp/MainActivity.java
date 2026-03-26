e4870bae3200e54b57162f2fec2f09636a5ecf5f
Oussaid <oussaid.dridi.0@gmail.com>
2026-03-26 19:57:03 +0100

    Initial commit - Android login UI

---
+++ app/src/main/java/com/example/tp/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.tp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button getStartedBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        getStartedBtn = findViewById(R.id.getStartedBtn);
+
+        getStartedBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, login.class);
+                startActivity(i);
+                finish();
+            }
+        });
+    }
+
+
+
+
+}
