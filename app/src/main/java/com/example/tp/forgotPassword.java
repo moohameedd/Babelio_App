a75812dc0b4fa61083eae3b763fbae9b6488245d
Oussaid <oussaid.dridi.0@gmail.com>
2026-03-29 17:40:00 +0100

    Add login, signup, and password reset activities with corresponding layouts

---
+++ app/src/main/java/com/example/tp/forgotPassword.java
@@ -0,0 +1,47 @@
+package com.example.tp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class login extends AppCompatActivity {
+    ImageView btnClose;
+    TextView forgotPassword;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+
+
+        btnClose = findViewById(R.id.btnClose);
+        forgotPassword = findViewById(R.id.forgotPassword);
+
+        forgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i =  new Intent(login.this, forgotPassword.class);
+                startActivity(i);
+            }
+        });
+
+        btnClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goBack = new Intent(login.this, MainActivity.class);
+                startActivity(goBack);
+                finish();
+            }
+        });
+
+    }
+}
